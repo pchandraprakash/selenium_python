70ee202d077d934ca3af2e798ec3aa195bbd0e28
Phanindra Chandraprakash <36249367+pchandraprakash@users.noreply.github.com>
2019-07-10 22:38:51 -0600

    Add files via upload

---
+++ Understanding_Variables_Data_Types/Strings.py
@@ -0,0 +1,20 @@
+"""
+Strings are sequence of characters
+String literals may be delimited using either single or double quotes.
+"""
+a = "this is a string with double quotes"
+b = 'this is a string with single quotes'
+print(a)
+print(b)
+
+c = 'there is a \'quote\' in this string'
+d = "there is a 'quote' in this string"
+e = 'there is a "quote" in this string'
+print(c)
+print(d)
+print(e)
+
+print("***********")
+print('foo\\bar')
+print('foo\tbar')
+print('foo\nbar')
